dbd1fdc7c07a0adc1edde6ea1b5ca16bbd589977
Astrospence <illustrator.wood@gmail.com>
2022-01-14 00:36:06 -0700

    few changes to ensure first 3 projects endpoints pass tests

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -29,11 +29,10 @@ router.get('/:id', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
     try {
-        const { name, description } = req.body
-        const newProject = Projects.insert({ name, description })
-        if (!name || !description) {
+        if (!req.body.name || !req.body.description) {
             res.status(400).json({ message: 'Please provide name and description'})
         } else {
+            const newProject = await Projects.insert(req.body)
             res.status(200).json(newProject)
         }
     } catch (err) {
