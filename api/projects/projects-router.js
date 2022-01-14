46a312bc7f0f2f86fc98301a36c0a520c6ac2d11
Astrospence <illustrator.wood@gmail.com>
2022-01-14 00:07:25 -0700

    there seems to be an issue with SQLITE per get error message-moving on for now

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,19 @@
 const express = require('express')
-const projects = require('./projects-model')
+const Projects = require('./projects-model')
 const router = express.Router()
 
-
+router.get('/', (req, res) => {
+    Projects.get()
+    .then(projects => {
+        if (!projects) {
+            console.log('nothin here')
+        } else {
+            console.log('here is the list')
+        }
+    })
+    .catch(err => {
+        res.status(500).json({ message: err.message })
+    })
+})
 
 module.exports = router
