8c35254cdaca6c65a4a043fc4a8acb203e543222
Astrospence <illustrator.wood@gmail.com>
2022-01-14 00:19:30 -0700

    wrote get project by id endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,18 +2,33 @@ const express = require('express')
 const Projects = require('./projects-model')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
     Projects.get()
     .then(projects => {
         if (!projects) {
-            console.log('nothin here')
+            res.status(200).json([])
         } else {
-            console.log('here is the list')
+            res.status(200).json(projects)
         }
     })
-    .catch(err => {
-        res.status(500).json({ message: err.message })
-    })
+    .catch(next)
+})
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const project = await Projects.get(req.params.id)
+        if (!project) {
+            res.status(404).json({ message: 'No project with specified id'})
+        } else {
+            res.status(200).json(project)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.use((err, req, res, next) => {
+    res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = router
