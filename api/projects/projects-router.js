c9cc0d1ba5e86e5852f7ea01b0c0bac990a282f8
Astrospence <illustrator.wood@gmail.com>
2022-01-14 00:56:18 -0700

    wrote projects put endpoint

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -30,10 +30,27 @@ router.get('/:id', async (req, res, next) => {
 router.post('/', async (req, res, next) => {
     try {
         if (!req.body.name || !req.body.description) {
-            res.status(400).json({ message: 'Please provide name and description'})
+            res.status(400).json({ message: 'Please provide name and description' })
         } else {
             const newProject = await Projects.insert(req.body)
             res.status(200).json(newProject)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    try {
+        if (!req.body.name || !req.body.description) {
+            res.status(400).json({ message: 'Please provide name and description' })
+        } else {
+            const updatedProject = await Projects.update(req.params.id, req.body)
+            if (!updatedProject) {
+                res.status(404).json({ message: 'No project with specified id'})
+            } else {
+                res.status(200).json(updatedProject)
+            }
         }
     } catch (err) {
         next(err)
